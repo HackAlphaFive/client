31d65309fb20869fa1f0ca8d8154900a6a751eaa
Kargobober <kargobober@yandex.ru>
2024-01-21 02:34:31 +0300

    refactor: move files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import Layout from '../Layout/Layout';
-import HomePage from '../HomePage/HomePage';
-import NotFound404Page from '../NotFound404Page/NotFound404Page';
-import IPRPage from '../IPRPage/IPRPage';
+import HomePage from '../../pages/HomePage/HomePage';
+import NotFound404Page from '../../pages/NotFound404Page/NotFound404Page';
+import IPRPage from '../../pages/IPRPage/IPRPage';
 
 function App(): JSX.Element {
   return (
