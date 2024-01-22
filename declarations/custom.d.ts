dfe670789977c3be6889f2cdd9a894878f232cf9
Alexey Tutrin <tuxone@yandex.ru>
2024-01-22 15:26:36 +0400

    feat: make header

---
+++ declarations/custom.d.ts
@@ -0,0 +1,28 @@
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+      React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png";
+
+declare module "*.png" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.png" {
+    const value: any;
+    export =value;
+}
+
