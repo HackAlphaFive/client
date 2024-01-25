ae276aba4118d15c429f84596e612eab07a48719
Kargobober <kargobober@yandex.ru>
2024-01-25 15:12:37 +0300

    feat: tab for filtration and filter icon like FC feat: add function "translateStatus" style: add font-weight 400 in index.html docs: types for → typography and color, statuses, icons's dimensions
    TODO: tab for filtration continued

---
+++ src/components/TabSortStatus/TabSortStatus.tsx
@@ -0,0 +1,75 @@
+import { useState, useEffect, useRef } from 'react';
+import { SelectDesktop } from '@alfalab/core-components/select/desktop';
+import { DimensionsListIcons, StatusList, StatusListRU } from '../../utils/types';
+import FilterDropDown from '../Icons/FilterDropDown/FilterDropDown';
+import { translateStatus } from '../../utils/utils';
+import { Radio } from '@alfalab/core-components/radio';
+import styles from './TabSortStatus.module.css';
+import { Gap } from '@alfalab/core-components/gap';
+
+type TProps = {
+  popoverWidth?: string;
+  mode?: 'ipr' | 'task';
+}
+
+function TabSortStatusIPR({popoverWidth = '206px', mode = 'ipr'} : TProps): JSX.Element {
+  const options = [
+    // content рисуется в инпуте при выборе данного пункта
+    { key: '0', content: <div className={`${styles.item} text_color_main text_type_middle`}>Все статусы</div> },
+    { key: '1', content: <div className={`${styles.item} text_color_main text_type_middle`}>{StatusListRU.Failed}</div> },
+    { key: '2', content: <div className={`${styles.item} text_color_main text_type_middle`}>{StatusListRU.InProgress}</div> },
+    { key: '3', content: <div className={`${styles.item} text_color_main text_type_middle`}>{StatusListRU.NoStatus}</div> },
+    { key: '4', content: <div className={`${styles.done} ${styles.item} text_type_middle`}>{StatusListRU.Done}</div> },
+    { key: '5', content: <div className={`${styles.canceled} ${styles.item} text_type_middle`}>{StatusListRU.Canceled}</div> },
+  ];
+
+  const [selected, setSelected] = useState<StatusList | undefined>(undefined);
+  useEffect(() => console.log(selected));
+
+  const myRef = useRef<HTMLDivElement>();
+
+  return (
+    <SelectDesktop
+      options={options}
+      placeholder='Выберите статус'
+      label='Статус ИПР'
+      multiple={false}
+      block={true}
+      size='xl'
+      optionsListClassName={styles.popover}
+      Arrow={() => <FilterDropDown size={DimensionsListIcons.l} />}
+      Option={({
+        option,
+        selected,
+        innerProps
+      }) => {
+        if (mode === 'task' && option.key === '3') {
+          return null
+        } else {
+          return (
+            <div style={{width: popoverWidth}}>
+              <Radio
+                size='m'
+                checked={selected}
+                label={option.content}
+                block={true}
+                align='center'
+                // @ts-ignore
+                labelProps={innerProps}
+                circleClassName={styles.circle}
+              />
+              <Gap size='l'></Gap>
+            </div>)
+        }
+      }}
+      onChange={(payload) => {
+        // SelectDesktop прокидывает сюда пропс payload, внутри которого хранятся данные о выборе
+        const key = payload.selected?.key;
+        const value = options.filter(option => option.key === key)[0].content.props.children as StatusListRU;
+        setSelected(translateStatus(value, 'ru-en') as StatusList | undefined);
+      }}
+    />
+  )
+}
+
+export default TabSortStatusIPR;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,57 @@
 import { v4 as uuidv4 } from 'uuid';
+import { StatusList, StatusListRU } from './types';
 
 // Для генерации уникальной айди используем нашу функцию, а не прямой вызов uuidv4
 export const getUniqId = () => uuidv4();
-
 
 
 type TErrorHandler = (text: string, error?: unknown) => void;
 
 export const handleError: TErrorHandler = (text, error = '') => {
   console.log(text, error);
+};
+
+
+export const translateStatus = (status: StatusList | StatusListRU, mode: 'ru-en' | 'en-ru') => {
+  if (mode === 'en-ru') {
+    let result;
+    switch (status) {
+      case StatusList.NoStatus:
+        result = StatusListRU.NoStatus;
+        break;
+      case StatusList.Failed:
+        result = StatusListRU.Failed;
+        break;
+      case StatusList.InProgress:
+        result = StatusListRU.InProgress;
+        break;
+      case StatusList.Done:
+        result = StatusListRU.Done;
+        break;
+      case StatusList.Canceled:
+        result = StatusListRU.Canceled;
+        break;
+    }
+    return result;
+  } else {
+    let result;
+    switch (status) {
+      case StatusListRU.NoStatus:
+        result = StatusList.NoStatus;
+        break;
+      case StatusListRU.Failed:
+        result = StatusList.Failed;
+        break;
+      case StatusListRU.InProgress:
+        result = StatusList.InProgress;
+        break;
+      case StatusListRU.Done:
+        result = StatusList.Done;
+        break;
+      case StatusListRU.Canceled:
+        result = StatusList.Canceled;
+        break;
+    }
+    return result;
+  }
 };
