75127b3f50bc19fc19b078b61eaef54cc5ee616c
Kargobober <kargobober@yandex.ru>
2024-01-29 20:50:15 +0300

    fix: spa-github-pages https://create-react-app.dev/docs/deployment/#github-pages https://github.com/rafgraph/spa-github-pages

--- src/pages/IPRPage/IPRPage.tsx
+++ src/pages/IPRPage/IPRPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Outlet } from 'react-router';
 
 function IPRPage() {
   return (
     <div style={{width: '222px'}}>
       <div>IPRPage</div>
+      <Outlet />
     </div>
   )
 }
